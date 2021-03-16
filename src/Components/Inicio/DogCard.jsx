d3772c0ff4736da1c24b7ec49a3819d38355f18c
geraldin <geraldinpinzonb@gmail.com>
2021-03-16 08:46:25 -0500

    Se crea modal

--- src/Components/Inicio/DogCard.jsx
+++ src/Components/Inicio/DogCard.jsx
@@ -1,8 +1,24 @@
-import React from "react";
-import { Image, Typography, Divider, Button } from "antd";
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+import { Image, Typography, Divider } from "antd";
 const { Title } = Typography;
-const DogCard = ({src,name,description}) => {
+const DogCard = ({src,name,description, description2}) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+  
+    const showModal = () => {
+      setIsModalVisible(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
+  
   return (
+    
     <div className="dog-card">
       <div className="dog-card-image">
         <Image
@@ -26,12 +42,18 @@ const DogCard = ({src,name,description}) => {
         </p>
         <br />
         <div style={{ textAlign: "center", padding: "10px" }}>
-          <Button id="profile-buttons" style={{ width: "40%" }}  size="large">
+          <Button id="profile-buttons" style={{ width: "40%" }}  size="large " onClick={showModal}>
             Ver más
           </Button>
+          <Modal style={{margin: "auto", fontWeight:'bold', textAlign:'justify'}}  title="Mi Historia" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}  >
+            <p>{description2}</p>
+          </Modal>
+         
+          
         </div>
       </div>
     </div>
+    
   );
 };
 
