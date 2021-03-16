1f7f665635c7e8ebdc9c9803486a7bcf82b090ce
Sebastian Calderon <jhonnseca0599@gmail.com>
2021-03-16 18:08:02 -0500

    Estructura de modal

--- src/Components/Inicio/DogCard.jsx
+++ src/Components/Inicio/DogCard.jsx
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
 import { Image, Typography, Divider } from "antd";
 const { Title } = Typography;
-const DogCard = ({src,name,description, description2}) => {
-    const [isModalVisible, setIsModalVisible] = useState(false);
+const DogCard = ({ 
+  src, 
+  name, 
+  ExternalDescription, 
+  InternalDescription 
+  }) => {
   
-    const showModal = () => {
-      setIsModalVisible(true);
-    };
-  
-    const handleOk = () => {
-      setIsModalVisible(false);
-    };
-  
-    const handleCancel = () => {
-      setIsModalVisible(false);
-    };
-  
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
   return (
-    
     <div className="dog-card">
       <div className="dog-card-image">
         <Image
-          style={{ borderRadius: "100%", width: "250px", height: "250px"}}
+          style={{ borderRadius: "100%", width: "250px", height: "250px" }}
           src={src}
           alt="picsum"
         />
@@ -33,27 +30,36 @@ const DogCard = ({src,name,description, description2}) => {
           <Title style={{ color: "#423834" }}>{name}</Title>
         </Divider>
         <br />
-        <p
-          style={{ textAlign: "center", margin: "auto"}}
-        >
-          <b>
-            {description}
-          </b>
+        <p style={{ textAlign: "center", margin: "auto" }}>
+          <b>{ExternalDescription}</b>
         </p>
         <br />
         <div style={{ textAlign: "center", padding: "10px" }}>
-          <Button id="profile-buttons" style={{ width: "40%" }}  size="large " onClick={showModal}>
+          <Button
+            id="profile-buttons"
+            style={{ width: "40%" }}
+            size="large "
+            onClick={showModal}
+          >
             Ver más
           </Button>
-          <Modal style={{margin: "auto", fontWeight:'bold', textAlign:'justify'}}  title="Mi Historia" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}  >
-            <p>{description2}</p>
+          <Modal
+            centered
+            title={null}
+            footer={null}
+            closable={false}
+            onCancel={showModal}
+            bodyStyle={{ backgroundColor: "white" }}
+            visible={isModalVisible}
+            width="60vw"
+          >
+            <div>
+              <p>{InternalDescription}</p>
+            </div>
           </Modal>
-         
-          
         </div>
       </div>
     </div>
-    
   );
 };
 
--- src/Components/Inicio/DogProfiles.jsx
+++ src/Components/Inicio/DogProfiles.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import DogCard from "./DogCard.jsx";
+import { DogsInfo } from "./StaticInfo/DogsInfo";
 import { Typography, Divider } from "antd";
 import "antd/dist/antd.css";
 const { Title } = Typography;
 const DogProfiles = () => {
   return (
     <div className="dog-profiles-container">
-      <div className="dog-profiles-title">  
-        <Title style={{ color: "#423834", textAlign: "center",fontSize:"46px" }}>
+      <div className="dog-profiles-title">
+        <Title
+          style={{ color: "#423834", textAlign: "center", fontSize: "46px" }}
+        >
           ¡Conoce a nuestros mejores amigos!
         </Title>
-        <Divider style={{ borderColor: "#423834" }}>
-        </Divider>
+        <Divider style={{ borderColor: "#423834" }}></Divider>
       </div>
       <br />
       <div className="dog-profiles-cards">
-        <DogCard src={"https://scontent.fbog2-1.fna.fbcdn.net/v/t1.0-9/69864362_523188741848265_2122992941319323648_o.jpg?_nc_cat=102&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=nnFJTf5MK-UAX_VbjMA&_nc_ht=scontent.fbog2-1.fna&oh=1ea4127089e425691091ccf93975ef5a&oe=6072979D"} name={"Manchas"} description2={"BUSCO UN HOGAR💙🐾 Es un perro dulce y sumiso con humanos y otros animales, es muy cariñoso y tranquilo, está acostumbrado a estar en casa, no hace daños y avisa para ir al baño. Es criollo, un cruce con pastor alemán aparentemente, tiene su colita recortada, creemos que tiene 1-2 años, los dientes están perfectos al igual que su pelaje. Lo encontramos en un parque cerca a la clínica reina Sofia en Bogotá hace unos 4 días, pero ha deambulado por la ciudad ya que otras personas lo tuvieron un tiempo en villa alzacia, luego lo perdieron de vista. Lo acogimos, desparasitamos y pusimos medicamento para las pulgas, no sabemos si tiene vacunas pero se las vamos a poner a penas el veterinario de el visto bueno para más información llamar al 3166271457📲"}/>
-        <DogCard src={"https://scontent.fbog2-2.fna.fbcdn.net/v/t1.0-9/69851708_523188791848260_5480161081789775872_o.jpg?_nc_cat=103&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=Eb4DhlUaDB4AX_n5uaT&_nc_ht=scontent.fbog2-2.fna&oh=aafbc875db6f721853e755e50a348e9f&oe=60723A60"} name={"Ofelia"} description2={"BUSCO UN HOGAR💙🐾 Es un perro dulce y sumiso con humanos y otros animales, es muy cariñoso y tranquilo, está acostumbrado a estar en casa, no hace daños y avisa para ir al baño. Es criollo, un cruce con pastor alemán aparentemente, tiene su colita recortada, creemos que tiene 1-2 años, los dientes están perfectos al igual que su pelaje. Lo encontramos en un parque cerca a la clínica reina Sofia en Bogotá hace unos 4 días, pero ha deambulado por la ciudad ya que otras personas lo tuvieron un tiempo en villa alzacia, luego lo perdieron de vista. Lo acogimos, desparasitamos y pusimos medicamento para las pulgas, no sabemos si tiene vacunas pero se las vamos a poner a penas el veterinario de el visto bueno para más información llamar al 3166271457📲"}/>
-        <DogCard src={"https://scontent.fbog2-1.fna.fbcdn.net/v/t1.0-9/69539109_523188785181594_7964906027390861312_o.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=glGJ9iKWE70AX-DPdlf&_nc_ht=scontent.fbog2-1.fna&oh=e48768d40257f62d11e49f7a73c2ccad&oe=60717BFB"} name={"Peluche"} description2={"BUSCO UN HOGAR💙🐾 Es un perro dulce y sumiso con humanos y otros animales, es muy cariñoso y tranquilo, está acostumbrado a estar en casa, no hace daños y avisa para ir al baño. Es criollo, un cruce con pastor alemán aparentemente, tiene su colita recortada, creemos que tiene 1-2 años, los dientes están perfectos al igual que su pelaje. Lo encontramos en un parque cerca a la clínica reina Sofia en Bogotá hace unos 4 días, pero ha deambulado por la ciudad ya que otras personas lo tuvieron un tiempo en villa alzacia, luego lo perdieron de vista. Lo acogimos, desparasitamos y pusimos medicamento para las pulgas, no sabemos si tiene vacunas pero se las vamos a poner a penas el veterinario de el visto bueno para más información llamar al 3166271457📲"}/>
+        {DogsInfo.map((DogProfile) => (
+          <DogCard key={DogProfile.name} {...DogProfile} />
+        ))}
       </div>
     </div>
   );
 };
-
 
 export default DogProfiles;
