35168d3977dff0de6e80b1dd69c3369b440bb351
geraldin <geraldinpinzonb@gmail.com>
2021-03-26 00:39:51 -0500

    Mejora modal

--- src/Components/Inicio/DogCard.jsx
+++ src/Components/Inicio/DogCard.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import { Modal, Button } from "antd";
-import { Image, Typography, Divider } from "antd";
+import { Image, Typography, Divider, Carousel  } from "antd";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 const { Title } = Typography;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+
 const DogCard = ({ 
   src, 
   name, 
   ExternalDescription, 
-  InternalDescription 
+  InternalDescription ,
+  galeryArray
   }) => {
   
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -15,53 +25,84 @@ const DogCard = ({
     setIsModalVisible(!isModalVisible);
   };
 
-  return (
-    <div className="dog-card">
-      <div className="dog-card-image">
-        <Image
-          style={{borderRadius: "100%", width: "250px", height: "250px" }}
-          src={src}
-          alt="picsum"
-/*        fallback
- */        />
-      </div>
-      <br />
-      <div className="dog-card-content">
-        <Divider style={{ borderColor: "#423834" }} dashed>
-          <Title style={{ color: "#423834" }}>{name}</Title>
-        </Divider>
+    return (
+      <div className="dog-card">
+        <div className="dog-card-image">
+          <Image
+            style={{borderRadius: "100%", width: "250px", height: "250px" }}
+            src={src}
+            alt="picsum"
+  /*        fallback
+  */        />
+        </div>
         <br />
-        <div style={{ textAlign: "center" }}>
-        <p style={{ textAlign: "center", margin: "auto" }}>
-          <b>{ExternalDescription}</b>
-        </p>
-        <br/>
-          <Button
-            id="profile-buttons"
-            style={{ width: "40%" }}
-            size="large "
-            onClick={showModal}
-          >
-            Ver más
-          </Button>
-          <Modal
-            centered
-            title={null}
-            footer={null}
-            closable={false}
-            onCancel={showModal}
-            bodyStyle={{ backgroundColor: "white" }}
-            visible={isModalVisible}
-            width="60vw"
-          >
+        <div className="dog-card-content">
+          <Divider style={{ borderColor: "#423834" }} dashed>
+            <Title style={{ color: "#423834" }}>{name}</Title>
+          </Divider>
+          <br />
+          <div style={{ textAlign: "center" }}>
+          <p style={{ textAlign: "center", margin: "auto" }}>
+            <b>{ExternalDescription}</b>
+          </p>
+          <br/>
+            <Button
+              id="profile-buttons"
+              style={{ width: "40%" }}
+              size="large "
+              onClick={showModal}
+            >
+              Ver más
+            </Button>
             <div>
-              <p>{InternalDescription}</p>
+              <Modal
+              centered
+              title={null}
+              footer={null}
+              closable={false}
+              onCancel={showModal}
+              
+              visible={isModalVisible}
+              width="60vw"
+            >
+              <div className={useStyles.root}>
+                <Grid container spacing={2} back style={{ borderRadius:"20px"}}>
+                  <Grid item xs={6}>
+                    <Carousel autoplay> 
+                    <div > 
+                    <Image
+                      preview={false}
+                      style={{borderRadius: "20px", width: "350px", height: "400px",
+                      alignItems:"center",
+                      color: "#fff",
+                      lineHeight: "160px",}}
+                      src={galeryArray[0]}
+                      alt="picsum"
+                    />
+                    </div>
+                    <div>
+                    <Image
+                      preview={false}
+                      style={{borderRadius: "20px", width: "350px", height: "400px",
+                      color: "#fff",
+                      lineHeight: "160px",}}
+                      src={galeryArray[1]}
+                      alt="picsum"
+                    />
+                    </div>
+                    </Carousel>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <p>{InternalDescription}</p>
+                  </Grid>
+                  </Grid>
+                </div>              
+              </Modal>
             </div>
-          </Modal>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default DogCard;
