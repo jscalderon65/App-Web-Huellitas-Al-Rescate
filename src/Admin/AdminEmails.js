355f6fe61cc4441c5bc371e68e786b11b7a605b1
Sebastian Calderon <jhonnseca0599@gmail.com>
2021-08-04 21:16:55 -0500

    new admin

--- src/Admin/AdminEmails.js
+++ src/Admin/AdminEmails.js
@@ -2,6 +2,7 @@ const AdminEmails = [
 "huellitasalrescateprae@gmail.com", 
 "geraldinpinzonb@gmail.com", 
 "jairprada9@gmail.com", 
-"jhonnseca0599@gmail.com"
+"jhonnseca0599@gmail.com",
+"brayan12r@gmail.com"
 ];
 export { AdminEmails }
